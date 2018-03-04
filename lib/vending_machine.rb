56af8e267672f7a167fcd20135c06f837f5ed6d7
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-04 21:58:37 +0000

    Add change_hash method

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -7,6 +7,10 @@ class VendingMachine
   def initialize
     @stock = Stock.new
     @coins = CoinBank.new
-    @change = 0
+    @change = change_hash
+  end
+
+  def change_hash
+    {"1"=>0, "2"=>0, "5"=>0, "10"=>0, "20"=>0, "50"=>0, "100"=>0, "200"=>0}
   end
 end
