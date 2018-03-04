0596e5679058bf8109e29902e53996049d555214
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-04 22:06:40 +0000

    Add 'Adding invalid coin' test

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -10,5 +10,10 @@ describe VendingMachine do
       vending_machine.add_coin(coin)
       expect(vending_machine.change[coin]).to eq 1
     end
+
+    it 'Adding invalid coin' do
+      coin = "30"
+      expect{vending_machine.add_coin(coin)}.to raise_error "The coin is invalid and cannot be added"
+    end
   end
 end
