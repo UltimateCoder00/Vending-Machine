fbbd2f888652009128618b2b3b37f7bc26a0004e
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-04 22:03:10 +0000

    Add 'Add change' method

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -3,4 +3,12 @@ require 'vending_machine'
 describe VendingMachine do
   subject(:vending_machine) {described_class.new}
 
+  describe '#add_coin' do
+    it 'Add change' do
+      coin = "50"
+      expect(vending_machine.change[coin]).to eq 0
+      vending_machine.add_coin(coin)
+      expect(vending_machine.change[coin]).to eq 1
+    end
+  end
 end
