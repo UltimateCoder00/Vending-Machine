61fba5f218b83cfdb354d91038d3c1f5fe5afc9e
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-04 16:27:55 +0000

    Add #remove Test

--- spec/stock_spec.rb
+++ spec/stock_spec.rb
@@ -4,11 +4,20 @@ describe Stock do
   subject(:stock) {described_class.new}
 
   describe '#add' do
-    it 'Adds product to stock' do
+    it 'Add product to stock' do
       product = "Coca Cola"
       quantity = stock.quantities[product]
       stock.add(product)
       expect(stock.quantities[product]).to eq quantity + 1
+    end
+  end
+
+  describe '#remove' do
+    it 'Remove product from stock' do
+      product = "Coca Cola"
+      quantity = stock.quantities[product]
+      stock.remove(product)
+      expect(stock.quantities[product]).to eq quantity - 1
     end
   end
 end
