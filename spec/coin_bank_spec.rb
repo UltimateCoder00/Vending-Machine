6113870d4f4caca57e66b94d0246920694bb0e88
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-04 20:11:30 +0000

    Add 'Add coin to coin bank' test

--- spec/coin_bank_spec.rb
+++ spec/coin_bank_spec.rb
@@ -3,4 +3,12 @@ require 'coin_bank'
 describe CoinBank do
   subject(:coin_bank) {described_class.new}
 
+  describe '#add' do
+    it 'Add coin to coin bank' do
+      coin = "50"
+      quantity = coin_bank.quantities[coin]
+      coin_bank.add(coin)
+      expect(coin_bank.quantities[coin]).to eq quantity + 1
+    end
+  end
 end
