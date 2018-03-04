2dd55f0c8483aa9f84478b94f900d1028d864d55
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-04 16:26:39 +0000

    Update initialize method

--- lib/stock.rb
+++ lib/stock.rb
@@ -4,8 +4,8 @@ class Stock
   attr_reader :prices, :quantities
 
   def initialize
-    @prices = CSV.new(File.new(Dir.pwd + '/stock_prices_db.csv')).to_h
-    @quantities = CSV.new(File.new(Dir.pwd + '/stock_quantities_db.csv')).to_h
+    @prices = CSV.new(File.new(Dir.pwd + '/lib/stock_prices_db.csv')).to_h
+    @quantities = CSV.new(File.new(Dir.pwd + '/lib/stock_quantities_db.csv')).to_h
     render_db
   end
 
