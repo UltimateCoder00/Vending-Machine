0292428257a82f8cc0c72ecb3fa9d2e4aec6f08c
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-04 13:26:11 +0000

    Add Initialize & render_db Methods

--- lib/stock.rb
+++ lib/stock.rb
@@ -1,5 +1,21 @@
-class Stock
-  def initialize
+require 'csv'
 
+class Stock
+  attr_reader :prices, :quantities
+
+  def initialize
+    @prices = CSV.new(File.new(Dir.pwd + '/stock_prices_db.csv')).to_h
+    @quantities = CSV.new(File.new(Dir.pwd + '/stock_quantities_db.csv')).to_h
+    render_db
+  end
+
+  def render_db
+    prices.each do |key, value|
+      prices[key] = value.to_f / 100
+    end
+
+    quantities.each do |key, value|
+      quantities[key] = value.to_i
+    end
   end
 end
