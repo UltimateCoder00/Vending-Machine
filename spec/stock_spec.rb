94fa0794d4291b6246d61881cc258b5e758f99ef
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-04 17:30:19 +0000

    Update tests to restrict to max allowed product adding

--- spec/stock_spec.rb
+++ spec/stock_spec.rb
@@ -6,6 +6,7 @@ describe Stock do
   describe '#add' do
     it 'Add product to stock' do
       product = "Coca Cola"
+      5.times {stock.remove(product)}
       quantity = stock.quantities[product]
       stock.add(product)
       expect(stock.quantities[product]).to eq quantity + 1
@@ -13,6 +14,7 @@ describe Stock do
 
     it 'Add multiple quantities of a product' do
       product = "Coca Cola"
+      6.times {stock.remove(product)}
       quantity = stock.quantities[product]
       quantity_number = 5
       stock.add(product, quantity_number)
@@ -22,6 +24,14 @@ describe Stock do
     it 'Adding invalid product to stock' do
       product = "Fizzy Cola"
       expect{stock.add(product)}.to raise_error "The product is invalid and cannot be added to stock"
+    end
+
+    it 'Add more product than max allowed' do
+      product = "Coca Cola"
+      5.times {stock.remove(product)}
+      quantity_number = 10
+      stock.add(product, quantity_number)
+      expect(stock.quantities[product]).to eq 10
     end
   end
 
