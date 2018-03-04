5bb3045f1d2534ad603775b53c47a8033312aa1a
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-04 20:12:34 +0000

    Add 'Add multiple quantities of a coin' test

--- spec/coin_bank_spec.rb
+++ spec/coin_bank_spec.rb
@@ -10,5 +10,13 @@ describe CoinBank do
       coin_bank.add(coin)
       expect(coin_bank.quantities[coin]).to eq quantity + 1
     end
+
+    it 'Add multiple quantities of a coin' do
+      coin = "50"
+      quantity = coin_bank.quantities[coin]
+      quantity_number = 5
+      coin_bank.add(coin, quantity_number)
+      expect(coin_bank.quantities[coin]).to eq quantity + quantity_number
+    end
   end
 end
