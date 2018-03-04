337eb5e659ae707406bbefb356cc4dd3cfcefc85
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-04 19:59:38 +0000

    Add render_db method

--- lib/coin_bank.rb
+++ lib/coin_bank.rb
@@ -5,5 +5,12 @@ class CoinBank
 
   def initialize
     @quantities = CSV.new(File.new(Dir.pwd + '/lib/coin_bank_db.csv')).to_h
+    render_db
+  end
+
+  private
+
+  def render_db
+    quantities.each { |k, v| quantities[k] = v.to_i }
   end
 end
