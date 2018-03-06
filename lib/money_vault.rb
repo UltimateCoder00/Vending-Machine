9373382972dae97787988fec12f70ddd92af0358
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-06 20:23:08 +0000

    Rename MoneyVault class content

--- lib/money_vault.rb
+++ lib/money_vault.rb
@@ -5,31 +5,31 @@ class MoneyVault
 
   def initialize
     @coin_stored_list = CSV.new(File.new(Dir.pwd + './db/coin_stored_list_db.csv')).to_h
-    render_db
+    render_coin_stored_db
   end
 
   def add(coin, quantity=1)
-    fail "The coin is invalid and cannot be added to bank" unless exists?(coin)
+    fail "The coin is invalid and cannot be added to bank" unless coin_exists?(coin)
     coin_stored_list[coin] += quantity
   end
 
   def remove(coin)
-    fail "The coin is invalid and cannot be removed from bank" unless exists?(coin)
-    fail "There is no coin to remove" if empty?(coin)
+    fail "The coin is invalid and cannot be removed from bank" unless coin_exists?(coin)
+    fail "There is no coin to remove" if coin_emptied?(coin)
     coin_stored_list[coin] -= 1
   end
 
   private
 
-  def render_db
+  def render_coin_stored_db
     coin_stored_list.each { |k, v| coin_stored_list[k] = v.to_i }
   end
 
-  def exists?(coin)
+  def coin_exists?(coin)
     coin_stored_list.include?(coin)
   end
 
-  def empty?(coin)
+  def coin_emptied?(coin)
     coin_stored_list[coin].zero?
   end
 end
