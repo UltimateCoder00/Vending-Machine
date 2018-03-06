7857d8ff943b4eb19547b09a368639b546988054
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-06 20:04:43 +0000

    Rename Stock class content

--- lib/stock.rb
+++ lib/stock.rb
@@ -2,49 +2,57 @@ require 'csv'
 
 class Stock
   attr_reader :price_list, :product_quantity_list
-  MAXPRODUCTSTOCK = 10
+  MAXPRODUCTQUANTITY = 10
 
   def initialize
-    @price_list = CSV.new(File.new(Dir.pwd + './db/stock_price_list_db.csv')).to_h
+    @price_list = CSV.new(File.new(Dir.pwd + './db/stock_product_price_list_db.csv')).to_h
     @product_quantity_list = CSV.new(File.new(Dir.pwd + './db/stock_product_quantity_list_db.csv')).to_h
-    render_db
+    render_stock_product_db
   end
 
   def add(product, quantity=1)
-    fail "The product is invalid and cannot be added to stock" unless exists?(product)
-    max?(product, quantity) ? product_quantity_list[product] = MAXPRODUCTSTOCK : product_quantity_list[product] += quantity
+    fail "The product is invalid and cannot be added to stock" unless product_exists?(product)
+    product_maxed?(product, quantity) ? max_product_quantity(product) : add_product(product, quantity)
   end
 
   def remove(product)
-    fail "The product is invalid and cannot be removed from stock" unless exists?(product)
-    fail "There is no product to remove" if empty?(product)
+    fail "The product is invalid and cannot be removed from stock" unless product_exists?(product)
+    fail "There is no product to remove" if product_empty?(product)
     product_quantity_list[product] -= 1
   end
 
   private
 
-  def render_db
-    render_prices_db
-    render_quantities_db
+  def render_stock_product_db
+    render_stock_product_price_list_db
+    render_stock_product_quantity_list_db
   end
 
-  def render_prices_db
+  def render_stock_product_price_list_db
     price_list.each { |k, v| price_list[k] = v.to_i }
   end
 
-  def render_quantities_db
+  def render_stock_product_quantity_list_db
     product_quantity_list.each { |k, v| product_quantity_list[k] = v.to_i }
   end
 
-  def exists?(product)
+  def product_exists?(product)
     price_list.include?(product)
   end
 
-  def empty?(product)
+  def max_product_quantity(product)
+    product_quantity_list[product] = MAXPRODUCTQUANTITY
+  end
+
+  def add_product(product, quantity)
+     product_quantity_list[product] += quantity
+  end
+
+  def product_empty?(product)
     product_quantity_list[product].zero?
   end
 
-  def max?(product, quantity)
-    product_quantity_list[product] + quantity > MAXPRODUCTSTOCK
+  def product_maxed?(product, quantity)
+    product_quantity_list[product] + quantity > MAXPRODUCTQUANTITY
   end
 end
